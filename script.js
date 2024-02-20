7d3ba2be2d5b36ea925c04ec391d4a404b6f67e3
Dharuv Manchanda <101541368+DharuvManchanda@users.noreply.github.com>
2024-02-20 15:52:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const toggleButton=document.getElementById('toggle');
+const navBar=document.getElementById("left-Nav");
+toggleButton.addEventListener('click', ()=>{
+    navBar.classList.toggle("active");  
+})
+console.log("dharuv");
